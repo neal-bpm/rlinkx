c5007bedceaecb778c2d11fd6ddeb0af24414ce5
neel vinoth <nealbpm6@gmail.com>
2025-01-22 07:38:03 -0500

    list all bookmarks in sidebar

--- lib/rlinkx_web/live/bookmark_live.ex
+++ lib/rlinkx_web/live/bookmark_live.ex
@@ -7,6 +7,23 @@ defmodule RlinkxWeb.BookmarkLive do
 
   def render(assigns) do
     ~H"""
+    <div class="flex flex-col shrink-0 w-64 bg-slate-100">
+      <div class="flex justify-between items-center shrink-0 h-16 border-b border-slate-300 px-4">
+        <div class="flex flex-col gap-1.5">
+          <h1 class="text-lg font-bold text-gray-800">
+            Rlinkx
+          </h1>
+        </div>
+      </div>
+      <div class="mt-4 overflow-auto">
+        <div class="flex items-center h-8 px-3">
+          <span class="ml-2 leading-none font-medium text-sm">Bookmarks</span>
+        </div>
+        <div id="bookmarks-list">
+          <.bookmark_link :for={bookmark <- @bookmarks} bookmark={bookmark} active={bookmark.id == @bookmark.id} />
+        </div>
+      </div>
+    </div>
     <div class="bg-[#34d399] flex flex-col grow shadow-lg">
       <div class="bg-[#34d300] flex justify-between items-center shrink-0 h-16 bg-white border-b border-slate-300 px-4">
         <div class="bg-[#34a200] flex flex-col gap-1.5">
@@ -29,10 +46,31 @@ defmodule RlinkxWeb.BookmarkLive do
     """
   end
 
-  def mount(_params, _session, socket) do
-    bookmark = Bookmark |> Repo.all |> List.first()
+  attr :active, :boolean, required: true
+  attr :bookmark, Bookmark, required: true
 
-    {:ok, assign(socket, hide_description?: false, bookmark: bookmark)}
+  defp bookmark_link(assigns) do
+    ~H"""
+    <a
+      class={[
+        "flex items-center h-8 text-sm pl-8 pr-3",
+        (@active && "bg-slate-300") || "hover:bg-slate-300"
+      ]}
+      href="#"
+    >
+        <.icon name="hero-hashtag" class="h-4 w-4" />
+        <span class={["ml-2 leading-none", @active && "font-bold"]}>
+          {@bookmark.name}
+        </span>
+    </a>
+    """
+  end
+
+  def mount(_params, _session, socket) do
+    bookmarks = Repo.all(Bookmark)
+    bookmark = List.first(bookmarks)
+
+    {:ok, assign(socket, hide_description?: false, bookmark: bookmark, bookmarks: bookmarks)}
   end
 
   def handle_event("toggle-description", _params, socket) do
