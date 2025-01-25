83da0a99d724b330c2e1ad0cd4b3f19a476a1e43
neel vinoth <nealbpm6@gmail.com>
2025-01-25 18:44:02 -0500

    use Ecto.Query in Remote context

--- lib/rlinkx/remote.ex
+++ lib/rlinkx/remote.ex
@@ -2,7 +2,9 @@ defmodule Rlinkx.Remote do
   alias Rlinkx.Remote.Bookmark
   alias Rlinkx.Repo
 
+  import Ecto.Query
+
   def list_bookmarks() do
-    Repo.all(Bookmark)
+    Repo.all(from Bookmark, order_by: [asc: :name])
   end
 end
